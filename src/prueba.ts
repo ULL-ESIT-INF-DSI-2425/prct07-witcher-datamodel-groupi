533ec7d405e3f72067974322c9e5f3102d12cf82
Ines Cabrera Betancor <alu0101548593@ull.edu.es>
2025-03-10 13:08:23 +0000

    codigo ejemplo para comprobar que la configuracion del repo es correcta

--- src/prueba.ts
+++ src/prueba.ts
@@ -1,1 +1,5 @@
 // Para que no pete
+
+export function Suma( number1 : number, number2 : number) : number {
+  return number1 + number2;
+}
--- tests/prueba.spec.ts
+++ tests/prueba.spec.ts
@@ -1,1 +1,9 @@
 // Para que comprobar cosas
+import {describe, test, expect} from "vitest"
+import {Suma} from "../src/prueba"
+
+describe("Para comprobar si coveralls y CI funciona", ()=> {
+  test("Funcion suma", ()=> {
+    expect(Suma(1,2)).toBe(3);
+  })
+})
