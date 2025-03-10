ab64dadfe68673a82605e28fea5a98972f3b5991
Ines Cabrera Betancor <alu0101548593@ull.edu.es>
2025-03-10 13:14:10 +0000

    README

--- tests/prueba.spec.ts
+++ tests/prueba.spec.ts
@@ -6,4 +6,10 @@ describe("Para comprobar si coveralls y CI funciona", ()=> {
   test("Funcion suma", ()=> {
     expect(Suma(1,2)).toBe(3);
   })
+  test("Funcion suma", ()=> {
+    expect(Suma(2,2)).toBe(4);
+  })
+  test("Funcion suma", ()=> {
+    expect(Suma(76,2)).toBe(78);
+  })
 })
